703368e34e84c2fad1bc618f3fbedd545a86398c
yingmu <wawamu2201@gmail.com>
2024-07-14 13:54:02 +0200

    modify layout + insert support assets folder and files

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@ import { Room } from "./Room";
 
 export default function Page() {
   return (
-    <Room>
-     <p>MUMU</p>
-      <p className="text-8xl">mumu</p>
-    </Room>
+    <div>
+      <p>MUMU</p>
+      <p className="text-8xl text-white" >mumu</p>
+    </div>
   );
 }
