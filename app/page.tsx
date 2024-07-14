5dcdd2512ec157ee0012299e3b4b875bb407491e
yingmu <wawamu2201@gmail.com>
2024-07-14 13:33:18 +0200

    install ui/shadcn + get styling works with tailwind etc

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 import { Room } from "./Room";
-import { CollaborativeApp } from "./CollaborativeApp";
 
 export default function Page() {
   return (
     <Room>
-      <CollaborativeApp />
+     <p>MUMU</p>
+      <p className="text-8xl">mumu</p>
     </Room>
   );
 }
